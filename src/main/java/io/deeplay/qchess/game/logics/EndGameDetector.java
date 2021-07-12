391125ab50f17f031294c4ed6ecc52bfe1e19998
Chae4ek <nominas.alexsey@gmail.com>
2021-07-12 14:55:18 +0600

    Исправлена проверка на ничью

--- src/main/java/io/deeplay/qchess/game/logics/EndGameDetector.java
+++ src/main/java/io/deeplay/qchess/game/logics/EndGameDetector.java
@@ -42,7 +42,7 @@ public class EndGameDetector {
         } else {
             ++pieceMoveCount;
         }
-        return pieceMoveCount != 50;
+        return pieceMoveCount == 50;
     }
 
     /**
