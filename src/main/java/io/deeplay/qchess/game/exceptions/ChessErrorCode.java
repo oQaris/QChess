bd14e3e5e1ced4426f3f48e58b96b7a4f5e38e9c
Manjidomoe <x67379@gmail.com>
2021-07-13 11:01:05 +0600

    Проблемы с merge

---
+++ src/main/java/io/deeplay/qchess/game/exceptions/ChessErrorCode.java
@@ -0,0 +1,25 @@
+package io.deeplay.qchess.game.exceptions;
+
+public enum ChessErrorCode {
+    INCORRECT_COORDINATES("Координаты выходят за границу доски"),
+    KING_NOT_FOUND("Король не найден"),
+    INCORRECT_FILLING_BOARD("Заполнение доски некорректное"),
+    INCORRECT_STRING_FOR_FILLING_BOARD("Заполнение из доски из некорректной строки"),
+    BOT_ERROR("В боте возникло исключение"),
+    CONSOLE_PLAYER_ERROR("Произошла ошибка в классе игрока"),
+    ERROR_WHEN_MOVING_FIGURE("Проверенный ход выдал ошибку при перемещении фигуры"),
+    ERROR_WHILE_CHECKING_FOR_DRAW("Ошибка при проверки на ничью"),
+    EXCEPTION_IN_HISTORY("Возникло исключение в истории"),
+    LOG_FAILED("Не удалось записать в лог"),
+    UNKNOWN_FIGURE_SELECTED("Выбрана неизвестная фигура");
+
+    private final String message;
+
+    ChessErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
