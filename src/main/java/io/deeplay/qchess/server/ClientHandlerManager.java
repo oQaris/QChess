f7320dbbb919ea8c1665c6a9b8f6b4a8fe424880
Chae4ek <nominas.alexsey@gmail.com>
2021-07-15 15:43:36 +0600

    Server template

---
+++ src/main/java/io/deeplay/qchess/server/ClientHandlerManager.java
@@ -0,0 +1,36 @@
+package io.deeplay.qchess.server;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ClientHandlerManager extends Thread {
+    private ServerSocket server;
+    private boolean stop;
+
+    public ClientHandlerManager(ServerSocket server) {
+        this.server = server;
+    }
+
+    public void terminate() {
+        stop = true;
+    }
+
+    @Override
+    public void run() {
+        while (!stop) {
+            try {
+                // TODO: make non-blocking code
+                Socket socket = server.accept();
+
+                if (socket != null) {
+                    ClientHandler client = new ClientHandler(socket);
+                    RPC.addClient(client);
+                    client.start();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println("Client was not processed!");
+            }
+        }
+    }
+}
