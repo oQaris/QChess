c4f9b27050e6ab6efecfee70aa040eadb19169b2
Chae4ek <nominas.alexsey@gmail.com>
2021-07-22 07:46:46 +0600

    Проект разбит на модули

--- core/src/main/java/io/deeplay/qchess/core/Main.java
+++ core/src/main/java/io/deeplay/qchess/core/Main.java
@@ -1,15 +1,16 @@
-package io.deeplay.qchess;
+package io.deeplay.qchess.core;
 
 import io.deeplay.qchess.client.view.IClientView;
 import io.deeplay.qchess.client.view.gui.ClientGUI;
-import io.deeplay.qchess.game.exceptions.ChessError;
 import io.deeplay.qchess.server.view.IServerView;
 import io.deeplay.qchess.server.view.ServerConsole;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ChessError, IOException {
+    public static void main(String[] args) throws IOException {
+        System.out.println(
+                "Введите \"s\", чтобы запустить сервер или \"c\", чтобы запустить клиент:");
         switch (new Scanner(System.in).nextLine()) {
             case "s" -> // server
             {
@@ -23,6 +24,7 @@ public class Main {
                 view.startView();
                 view.close();
             }
+            default -> System.out.println("Введен неверный запрос");
         }
     }
 }
---
+++ game/src/main/java/io/deeplay/qchess/game/Main.java
@@ -0,0 +1,25 @@
+package io.deeplay.qchess.game;
+
+import io.deeplay.qchess.game.exceptions.ChessError;
+import io.deeplay.qchess.game.model.Board;
+import io.deeplay.qchess.game.model.Color;
+import io.deeplay.qchess.game.player.ConsolePlayer;
+import io.deeplay.qchess.game.player.MinimaxBot;
+import io.deeplay.qchess.game.player.Player;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws ChessError, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        GameSettings roomSettings = new GameSettings(Board.BoardFilling.STANDARD);
+        Player firstPlayer = new ConsolePlayer(roomSettings, Color.WHITE, in);
+        Player secondPlayer = new MinimaxBot(roomSettings, Color.BLACK, 3);
+        Selfplay game = new Selfplay(roomSettings, firstPlayer, secondPlayer);
+        game.run();
+
+        in.close();
+    }
+}
