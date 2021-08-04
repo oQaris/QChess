45a0826b9af735d1c8be8294efc5d6fe3efa51b2
Chae4ek <nominas.alexsey@gmail.com>
2021-08-04 12:15:20 +0600

    чето

--- nnnbot/src/main/java/io/deeplay/qchess/nnnbot/bot/searchfunc/SearchFunc.java
+++
@@ -1,9 +0,0 @@
-package io.deeplay.qchess.nnnbot.bot.searchfunc;
-
-import io.deeplay.qchess.game.exceptions.ChessError;
-import io.deeplay.qchess.game.model.Move;
-
-@FunctionalInterface
-public interface SearchFunc {
-    Move findBest() throws ChessError;
-}
