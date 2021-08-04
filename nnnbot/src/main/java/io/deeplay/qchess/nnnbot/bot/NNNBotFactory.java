1389206caf87f22c57ea95dfdf0eff90df5a917e
Chae4ek <nominas.alexsey@gmail.com>
2021-08-04 12:18:25 +0600

    для реву

---
+++ nnnbot/src/main/java/io/deeplay/qchess/nnnbot/bot/NNNBotFactory.java
@@ -0,0 +1,46 @@
+package io.deeplay.qchess.nnnbot.bot;
+
+import io.deeplay.qchess.game.GameSettings;
+import io.deeplay.qchess.game.model.Color;
+import io.deeplay.qchess.nnnbot.bot.evaluationfunc.EvaluationFunc;
+import io.deeplay.qchess.nnnbot.bot.evaluationfunc.MatrixEvaluation;
+import io.deeplay.qchess.nnnbot.bot.searchfunc.parallelsearch.NegamaxAlfaBetaPruning;
+import io.deeplay.qchess.nnnbot.bot.searchfunc.parallelsearch.ParallelSearch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+public class NNNBotFactory {
+
+    private static final Logger logger = LoggerFactory.getLogger(NNNBotFactory.class);
+
+    private static String time;
+
+    private static int lastBotId;
+
+    /** Устанавливает время для записи логов */
+    public static void setTime(String time) {
+        NNNBotFactory.time = time;
+    }
+
+    public static synchronized NNNBot getNNNBot(GameSettings gs, Color color) {
+        MDC.put("time", time);
+
+        final int maxDepth = 2;
+        gs.history.setMinBoardStateToSave(maxDepth);
+
+        EvaluationFunc evaluationFunc = MatrixEvaluation::defenseHeuristics;
+        ParallelSearch deepSearch = new NegamaxAlfaBetaPruning(gs, color, evaluationFunc, maxDepth);
+
+        NNNBot nnnBot = new NNNBot(gs, color, deepSearch);
+
+        nnnBot.setId(++lastBotId);
+
+        logger.info(
+                "[NNNBotFactory] Создан бот #{} цвета {} с глубиной поиска {}",
+                lastBotId,
+                color,
+                deepSearch.maxDepth);
+        return nnnBot;
+    }
+}
---
+++ nnnbot/src/main/java/io/deeplay/qchess/nnnbot/bot/searchfunc/parallelsearch/ParallelSearch.java
@@ -0,0 +1,95 @@
+package io.deeplay.qchess.nnnbot.bot.searchfunc.parallelsearch;
+
+import io.deeplay.qchess.game.GameSettings;
+import io.deeplay.qchess.game.exceptions.ChessError;
+import io.deeplay.qchess.game.model.Color;
+import io.deeplay.qchess.game.model.Move;
+import io.deeplay.qchess.nnnbot.bot.evaluationfunc.EvaluationFunc;
+import io.deeplay.qchess.nnnbot.bot.searchfunc.SearchFunc;
+import java.util.List;
+
+/** Поиск с альфа-бета отсечением на заданную глубину */
+public abstract class ParallelSearch implements SearchFunc {
+
+    public final int maxDepth;
+
+    public final GameSettings gs;
+    public final EvaluationFunc evaluationFunc;
+    public final Color myColor;
+    public final Color enemyColor;
+
+    protected volatile Move theBestMove;
+    protected volatile double theBestEvaluation = Double.MIN_VALUE;
+    protected volatile double theBestDepth;
+
+    protected ParallelSearch(
+            GameSettings gs, Color color, EvaluationFunc evaluationFunc, int maxDepth) {
+        this.gs = gs;
+        this.evaluationFunc = evaluationFunc;
+        this.myColor = color;
+        this.enemyColor = color.inverse();
+        this.maxDepth = maxDepth;
+        this.theBestDepth = maxDepth;
+    }
+
+    protected synchronized void tryUpdateTheBestEvaluation(
+            Move move, double evaluation, int depth) {
+        if (depth <= theBestDepth && evaluation > theBestEvaluation) {
+            theBestEvaluation = evaluation;
+            theBestMove = move;
+            // TODO: top moves
+        }
+    }
+
+    @Override
+    public Move findBest() throws ChessError {
+        List<Move> allMoves = gs.board.getAllPreparedMoves(gs, myColor);
+        Move theBest = null;
+        double optEstimation = EvaluationFunc.MIN_ESTIMATION;
+
+        // TODO: запуск нескольких потоков для начальной глубины
+        for (Move move : allMoves) {
+            gs.moveSystem.move(move);
+            double estimation = run(false, maxDepth);
+            gs.moveSystem.undoMove();
+            if (estimation > optEstimation || theBest == null) {
+                optEstimation = estimation;
+                theBest = move;
+            }
+        }
+
+        return theBest;
+    }
+
+    /** @return лучшая оценка для текущего цвета myColor */
+    public abstract double run(boolean isMyMove, int depth) throws ChessError;
+
+    protected boolean isTerminalNode(List<Move> allMoves) {
+        return allMoves.isEmpty() || gs.endGameDetector.isDraw();
+    }
+
+    protected double getEvaluation(List<Move> allMoves, boolean isMyMove, int depth)
+            throws ChessError {
+        List<Move> allEnemyMoves;
+        List<Move> allMyMoves;
+        if (isMyMove) {
+            allMyMoves = allMoves;
+            if (allMyMoves.isEmpty()) return EvaluationFunc.MIN_ESTIMATION;
+
+            allEnemyMoves = gs.moveSystem.getAllCorrectMovesSilence(enemyColor);
+            if (allEnemyMoves.isEmpty() && gs.endGameDetector.isCheck(enemyColor))
+                return EvaluationFunc.MAX_ESTIMATION;
+        } else {
+            allEnemyMoves = allMoves;
+            if (allEnemyMoves.isEmpty() && gs.endGameDetector.isCheck(enemyColor))
+                return EvaluationFunc.MAX_ESTIMATION;
+
+            allMyMoves = gs.moveSystem.getAllCorrectMovesSilence(myColor);
+            if (allMyMoves.isEmpty()) return EvaluationFunc.MIN_ESTIMATION;
+        }
+
+        if (gs.endGameDetector.isDraw()) return EvaluationFunc.MIN_ESTIMATION;
+
+        return evaluationFunc.getHeuristics(gs, myColor);
+    }
+}
