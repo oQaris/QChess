68acad12c2d8b050cdfdc5f46afbd293b4452aed
Chae4ek <nominas.alexsey@gmail.com>
2021-08-08 02:16:24 +0600

    Исправлены возможные коллизии в истории ходов

--- game/src/main/java/io/deeplay/qchess/game/model/figures/FigureType.java
+++ game/src/main/java/io/deeplay/qchess/game/model/figures/FigureType.java
@@ -1,15 +1,15 @@
 package io.deeplay.qchess.game.model.figures;
 
 public enum FigureType {
-    BISHOP((byte) 0),
-    KING((byte) 1),
-    KNIGHT((byte) 2),
-    PAWN((byte) 3),
-    QUEEN((byte) 4),
-    ROOK((byte) 5);
+    BISHOP((byte) 1),
+    KING((byte) 2),
+    KNIGHT((byte) 3),
+    PAWN((byte) 4),
+    QUEEN((byte) 5),
+    ROOK((byte) 6);
 
     public static final String[] nameOfTypeNumber = {
-        "BISHOP", "KING", "KNIGHT", "PAWN", "QUEEN", "ROOK"
+        "NULL", "BISHOP", "KING", "KNIGHT", "PAWN", "QUEEN", "ROOK"
     };
 
     /** Нужен для быстрого вычисления состояния доски */
