0e1123efff2b70538c218b3e6ea40977854ec8df
Chae4ek <nominas.alexsey@gmail.com>
2021-08-09 03:14:20 +0600

    Обновлены матрицы для оценки позиции

--- nnnbot/src/main/java/io/deeplay/qchess/nnnbot/bot/evaluationfunc/evalimpl/MatrixEvaluation.java
+++ nnnbot/src/main/java/io/deeplay/qchess/nnnbot/bot/evaluationfunc/evalimpl/MatrixEvaluation.java
@@ -1,6 +1,7 @@
 package io.deeplay.qchess.nnnbot.bot.evaluationfunc.evalimpl;
 
 import io.deeplay.qchess.game.GameSettings;
+import io.deeplay.qchess.game.exceptions.ChessError;
 import io.deeplay.qchess.game.model.Color;
 import io.deeplay.qchess.game.model.figures.Figure;
 import io.deeplay.qchess.game.model.figures.FigureType;
@@ -9,78 +10,81 @@ import java.util.Map;
 
 public class MatrixEvaluation {
 
+    // ----------- Оценки позиции выбраны исходя из стоимости пешки == 100 ----------- //
+    // -------------- Матрицы выбраны так, что белый игрок играет снизу -------------- //
+
     private static final Integer[][] pawnEval = {
         {800, 800, 800, 800, 800, 800, 800, 800},
-        {290, 290, 290, 290, 290, 290, 290, 290},
-        {100, 100, 100, 100, 100, 100, 100, 100},
-        {20, 20, 20, 20, 20, 20, 20, 20},
-        {15, 15, 15, 15, 15, 15, 15, 15},
-        {11, 11, 11, 11, 11, 11, 11, 11},
+        {290, 285, 280, 270, 270, 280, 285, 290},
+        {12, 13, 15, 14, 14, 15, 13, 12},
+        {12, 13, 14, 15, 15, 14, 13, 12},
+        {12, 11, 15, 14, 15, 12, 11, 12},
+        {11, 12, 14, 13, 14, 11, 12, 11},
         {10, 10, 10, 10, 10, 10, 10, 10},
-        {9, 9, 9, 9, 9, 9, 9, 9},
+        {9, 9, 9, 9, 9, 9, 9, 9}
     };
     private static final Integer[][] knightEval = {
-        {65, 75, 85, 95, 95, 85, 75, 65},
-        {90, 100, 120, 100, 100, 120, 100, 90},
-        {80, 90, 100, 100, 100, 100, 90, 80},
-        {80, 85, 90, 90, 90, 90, 85, 80},
-        {75, 80, 85, 90, 90, 85, 80, 75},
-        {70, 75, 70, 75, 75, 70, 75, 70},
-        {50, 60, 75, 70, 70, 75, 60, 50},
         {40, 50, 60, 70, 70, 60, 50, 40},
+        {50, 60, 75, 70, 70, 75, 60, 50},
+        {70, 75, 80, 75, 75, 80, 75, 70},
+        {75, 80, 85, 90, 90, 85, 80, 75},
+        {75, 80, 85, 90, 90, 85, 80, 75},
+        {70, 75, 80, 75, 75, 80, 75, 70},
+        {50, 60, 75, 70, 70, 75, 60, 50},
+        {40, 50, 60, 70, 70, 60, 50, 40}
     };
     private static final Integer[][] bishopEval = {
-        {75, 75, 85, 95, 95, 85, 75, 75},
-        {80, 90, 100, 120, 120, 100, 90, 80},
-        {90, 100, 100, 95, 95, 100, 100, 90},
-        {100, 100, 90, 95, 95, 90, 100, 100},
-        {100, 90, 90, 85, 85, 90, 90, 100},
-        {85, 80, 80, 90, 90, 80, 80, 85},
+        {75, 75, 85, 100, 100, 85, 75, 75},
+        {80, 85, 90, 85, 85, 90, 85, 80},
+        {80, 80, 85, 90, 90, 85, 80, 80},
+        {85, 85, 90, 95, 95, 90, 85, 85},
+        {85, 85, 90, 95, 95, 90, 85, 85},
+        {80, 80, 80, 85, 85, 80, 80, 80},
         {80, 70, 60, 70, 70, 60, 70, 80},
-        {60, 50, 60, 50, 50, 60, 50, 60},
+        {60, 50, 60, 50, 50, 60, 50, 60}
     };
     private static final Integer[][] rookEval = {
-        {120, 120, 110, 130, 130, 110, 120, 120},
-        {90, 100, 100, 100, 100, 100, 100, 90},
-        {100, 110, 115, 110, 110, 115, 110, 100},
-        {110, 115, 120, 120, 120, 120, 115, 110},
-        {110, 115, 120, 120, 120, 120, 115, 110},
-        {90, 110, 115, 110, 110, 115, 110, 90},
-        {90, 105, 105, 110, 110, 105, 105, 90},
-        {100, 100, 100, 120, 120, 100, 100, 100},
+        {50, 60, 65, 70, 70, 65, 60, 50},
+        {40, 50, 50, 50, 50, 50, 50, 40},
+        {50, 60, 65, 60, 60, 65, 60, 50},
+        {60, 65, 70, 70, 70, 70, 65, 60},
+        {60, 65, 70, 70, 70, 70, 65, 60},
+        {40, 60, 65, 65, 65, 65, 60, 40},
+        {40, 55, 55, 60, 60, 55, 55, 40},
+        {50, 55, 55, 60, 60, 55, 55, 50}
     };
     private static final Integer[][] queenEval = {
-        {176, 178, 178, 179, 179, 178, 178, 176},
-        {178, 180, 180, 180, 180, 180, 180, 178},
-        {178, 180, 181, 181, 181, 181, 180, 178},
-        {179, 180, 181, 181, 181, 181, 180, 179},
-        {179, 180, 181, 181, 181, 181, 180, 179},
-        {178, 181, 181, 181, 181, 181, 180, 178},
-        {178, 180, 181, 180, 180, 180, 180, 178},
-        {176, 178, 178, 179, 179, 178, 178, 176},
+        {40, 45, 50, 50, 50, 50, 45, 40},
+        {45, 50, 55, 55, 55, 55, 50, 45},
+        {50, 60, 65, 65, 65, 65, 60, 50},
+        {60, 65, 70, 70, 70, 70, 65, 60},
+        {60, 65, 70, 70, 70, 70, 65, 60},
+        {50, 60, 65, 65, 65, 65, 60, 50},
+        {45, 50, 55, 55, 55, 55, 50, 45},
+        {40, 45, 50, 50, 50, 50, 45, 40}
     };
     private static final Integer[][] kingEval = {
-        {1794, 1792, 1792, 1790, 1790, 1792, 1792, 1794},
-        {1794, 1792, 1792, 1790, 1790, 1792, 1792, 1794},
-        {1794, 1792, 1792, 1790, 1790, 1792, 1792, 1794},
-        {1794, 1792, 1792, 1790, 1790, 1792, 1792, 1794},
-        {1796, 1794, 1794, 1792, 1792, 1794, 1794, 1796},
-        {1798, 1796, 1796, 1796, 1796, 1796, 1796, 1798},
-        {1804, 1804, 1800, 1800, 1800, 1800, 1804, 1804},
-        {1804, 1806, 1802, 1800, 1800, 1802, 1806, 1804},
+        {70, 70, 75, 75, 75, 75, 70, 70},
+        {70, 70, 70, 70, 70, 70, 70, 70},
+        {65, 60, 60, 55, 55, 60, 60, 65},
+        {65, 60, 55, 50, 50, 55, 60, 65},
+        {80, 70, 60, 50, 50, 60, 70, 80},
+        {90, 85, 80, 80, 80, 80, 85, 90},
+        {99, 99, 99, 99, 99, 99, 99, 99},
+        {99, 99, 99, 100, 100, 99, 99, 99}
     };
 
     private static final Map<FigureType, Integer[][]> evaluations;
-    private static final Map<FigureType, Integer> constInPawns;
+    private static final Map<FigureType, Integer> costInPawns;
 
     static {
-        constInPawns = new EnumMap<>(FigureType.class);
-        constInPawns.put(FigureType.PAWN, 100);
-        constInPawns.put(FigureType.KNIGHT, 320);
-        constInPawns.put(FigureType.BISHOP, 330);
-        constInPawns.put(FigureType.ROOK, 500);
-        constInPawns.put(FigureType.QUEEN, 900);
-        constInPawns.put(FigureType.KING, 20000);
+        costInPawns = new EnumMap<>(FigureType.class);
+        costInPawns.put(FigureType.PAWN, 100);
+        costInPawns.put(FigureType.KNIGHT, 320);
+        costInPawns.put(FigureType.BISHOP, 330);
+        costInPawns.put(FigureType.ROOK, 500);
+        costInPawns.put(FigureType.QUEEN, 900);
+        costInPawns.put(FigureType.KING, 20000);
     }
 
     static {
@@ -91,6 +95,8 @@ public class MatrixEvaluation {
         evaluations.put(FigureType.ROOK, rookEval);
         evaluations.put(FigureType.QUEEN, queenEval);
         evaluations.put(FigureType.KING, kingEval);
+
+        // TODO: добавить проверку, что стоимости фигур пропорциональны стоимости позиции
     }
 
     /**
@@ -107,7 +113,9 @@ public class MatrixEvaluation {
                 if (figure == null) continue;
                 int r = figure.getColor() == Color.BLACK ? 7 - row : row;
 
-                int eval = evaluations.get(figure.figureType)[r][column];
+                int eval =
+                        evaluations.get(figure.figureType)[r][column]
+                                + costInPawns.get(figure.figureType);
                 if (figure.getColor() == myColor) myEstimation += eval;
                 else enemyEstimation += eval;
             }
@@ -119,9 +127,9 @@ public class MatrixEvaluation {
      *
      * @param myColor цвет игрока, который атакует соперника
      */
-    public static int figureAttackHeuristics(GameSettings gs, Color myColor) {
-        int myMoveCount = gs.moveSystem.getMoveCounts(myColor);
-        int enemyMoveCount = gs.moveSystem.getMoveCounts(myColor.inverse());
+    public static int figureAttackHeuristics(GameSettings gs, Color myColor) throws ChessError {
+        int myMoveCount = gs.board.getAllPreparedMoves(gs, myColor).size();
+        int enemyMoveCount = gs.board.getAllPreparedMoves(gs, myColor.inverse()).size();
         return myMoveCount - enemyMoveCount;
     }
 
@@ -130,7 +138,7 @@ public class MatrixEvaluation {
      *
      * @param myColor цвет игрока, который атакует соперника
      */
-    public static int ultimateHeuristics(GameSettings gs, Color myColor) {
+    public static int ultimateHeuristics(GameSettings gs, Color myColor) throws ChessError {
         return figurePositionHeuristics(gs, myColor) + 20 * figureAttackHeuristics(gs, myColor);
     }
 }
