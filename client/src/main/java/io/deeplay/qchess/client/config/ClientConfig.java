e604eb183a5881239c2c1afcb9c1c0b8064946b0
Manjidomoe <x67379@gmail.com>
2021-08-10 12:11:05 +0600

    Методы валидации полей config файла выделены в отдельный сервис

--- client/src/main/java/io/deeplay/qchess/client/config/ClientConfig.java
+++ client/src/main/java/io/deeplay/qchess/client/config/ClientConfig.java
@@ -5,7 +5,6 @@ import io.deeplay.qchess.game.model.Color;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.regex.Pattern;
 
 public class ClientConfig {
 
@@ -26,14 +25,14 @@ public class ClientConfig {
         try {
             fis = new FileInputStream(configPath);
             property.load(fis);
-            ip = validateIp(property.getProperty("client.ip"));
-            port = validatePort(property.getProperty("client.port"));
-            isGui = validateBoolean(property.getProperty("client.isGui"));
-            playerType = validatePlayerType(property.getProperty("client.playerType"));
-            logPath = validatePath(property.getProperty("client.logPath"));
-            logBack = validatePath(property.getProperty("client.logBack"));
-            color = validateColor(property.getProperty("client.color"));
-            isTournament = validateBoolean(property.getProperty("isTournament"));
+            ip = ConfigService.validateIp(property.getProperty("client.ip"));
+            port = ConfigService.validatePort(property.getProperty("client.port"));
+            isGui = ConfigService.validateBoolean(property.getProperty("client.isGui"));
+            playerType = ConfigService.validatePlayerType(property.getProperty("client.playerType"));
+            logPath = ConfigService.validatePath(property.getProperty("client.logPath"));
+            logBack = ConfigService.validatePath(property.getProperty("client.logBack"));
+            color = ConfigService.validateColor(property.getProperty("client.color"));
+            isTournament = ConfigService.validateBoolean(property.getProperty("isTournament"));
 
         } catch (IOException e) {
             throw new ConfigException(ConfigExceptionEnum.READ_CONFIG_FILE);
@@ -44,156 +43,6 @@ public class ClientConfig {
 
     public ClientConfig() throws ConfigException {
         this(configPath);
-    }
-
-    /**
-     * Валидация значения полученного по ключу client.ip.
-     *
-     * @param property поле считанное с config файла по ключу client.ip.
-     * @return Результат property.
-     * @throws ConfigException выбросится если
-     *  - значение поля было пустым или ip вообще не было в config файле;
-     *  - ip состоит не из 4-х октетов;
-     *  - один из октетов не int;
-     *  - один из октетов выходит за диапазон [0..255].
-     */
-    private String validateIp(String property) throws ConfigException {
-        if (property == null || property.isEmpty()) {
-            throw new ConfigException(
-                ConfigExceptionEnum.ABSENT_IP);
-        }
-        String[] octets = property.split("\\.");
-        if (octets.length != 4) {
-            throw new ConfigException(ConfigExceptionEnum.INCORRECT_IP_OCTETS_NUMBER);
-        }
-        for (int i = 0; i < 4; i++) {
-            try {
-                int intOctet = Integer.parseInt(octets[i]);
-                if (intOctet < 0 || intOctet > 255) {
-                    throw new ConfigException(
-                       ConfigExceptionEnum.RANGE_OUT_IP_OCTET);
-                }
-            } catch (NumberFormatException e) {
-                throw new ConfigException(
-                    ConfigExceptionEnum.INCORRECT_IP_OCTET_VALUE);
-            }
-        }
-        return property;
-    }
-
-    /**
-     * Валидация значения полученного по ключу client.port.
-     *
-     * @param property поле считанное с config файла по ключу client.port.
-     * @return Результат целочисленное представление параметра property.
-     * @throws ConfigException выбросится если
-     *  - значение поля было пустым или port вообще не было в config файле;
-     *  - port не int;
-     *  - port является неположительным числом.
-     */
-    private int validatePort(String property) throws ConfigException {
-        if (property == null || property.isEmpty()) {
-            throw new ConfigException(
-                ConfigExceptionEnum.ABSENT_PORT);
-        }
-        try {
-            int port = Integer.parseInt(property);
-            if (port <= 0) {
-                throw new ConfigException(ConfigExceptionEnum.NON_POSITIVE_PORT_VALUE);
-            }
-            return port;
-        } catch (NumberFormatException e) {
-            throw new ConfigException(ConfigExceptionEnum.INCORRECT_PORT_VALUE);
-        }
-    }
-
-    /**
-     * Валидация boolean значения.
-     *
-     * @param property - boolean значение обёрнутое строкой.
-     * @return Результат boolean представление параметра property.
-     * @throws ConfigException выбросится если
-     *  - значение было пустым или null;
-     *  - property не явлеяется строкой "true" или "false".
-     */
-    private boolean validateBoolean(String property) throws ConfigException {
-        if (property == null || property.isEmpty()) {
-            throw new ConfigException(
-                ConfigExceptionEnum.ABSENT_BOOLEAN);
-        }
-        if (property.equals("true")) {
-            return true;
-        } else if (property.equals("false")) {
-            return false;
-        }
-        throw new ConfigException(ConfigExceptionEnum.INCORRECT_BOOLEAN_VALUE);
-    }
-
-    /**
-     * Валидация значения полученного по ключу client.playerType.
-     *
-     * @param property поле считанное с config файла по ключу client.playerType.
-     * @return Результат одно из значений enum client.view.gui.PlayerType.
-     * @throws ConfigException выбросится если
-     *  - значение поля было пустым или playerType вообще не было в config файле;
-     *  - неудалось распарсить значение в элемент enum client.view.gui.PlayerType.
-     */
-    private PlayerType validatePlayerType(String property) throws ConfigException {
-        if (property == null || property.isEmpty()) {
-            throw new ConfigException(
-                ConfigExceptionEnum.ABSENT_PLAYER_TYPE);
-        }
-        try {
-            return PlayerType.valueOf(property);
-        } catch (IllegalArgumentException e) {
-            throw new ConfigException(ConfigExceptionEnum.INCORRECT_PLAYER_TYPE_VALUE);
-        }
-    }
-
-    /**
-     * Валидация пути к папке
-     *
-     * @param property - строковое представление пути к папке.
-     * @return Результат property.
-     * @throws ConfigException выбросится если
-     *  - значение было пустым или null;
-     *  - путь не соответствует шаблону представления пути;
-     */
-    private String validatePath(String property) throws ConfigException {
-        if (property == null || property.isEmpty()) {
-            throw new ConfigException(
-               ConfigExceptionEnum.ABSENT_PATH);
-        }
-        String pattern = "/([\\w]+/)*";
-        if (Pattern.matches(pattern, property)) {
-            throw new ConfigException(
-                ConfigExceptionEnum.INCORRECT_PATH);
-        }
-        return property;
-    }
-
-    /**
-     * Валидация значения полученного по ключу client.color.
-     *
-     * @param property поле считанное с config файла по ключу client.color.
-     * @return Результат одно из значений enum game.model.Color или null (нужно выбрать рандомный цвет).
-     * @throws ConfigException выбросится если
-     *  - значение поля было пустым или color вообще не было в config файле;
-     *  - property не парсится ни в один из цветов, а также не является строкой "RANDOM".
-     */
-    private Color validateColor(String property) throws ConfigException {
-        if (property == null || property.isEmpty()) {
-            throw new ConfigException(
-                ConfigExceptionEnum.ABSENT_COLOR);
-        }
-        if (property.equals("WHITE")) {
-            return Color.WHITE;
-        } else if (property.equals("BLACK")) {
-            return Color.BLACK;
-        } else if (property.equals("RANDOM")) {
-            return null;
-        }
-        throw new ConfigException(ConfigExceptionEnum.INCORRECT_COLOR);
     }
 
     public static String getConfigPath() {
---
+++ client/src/main/java/io/deeplay/qchess/client/config/ConfigService.java
@@ -0,0 +1,158 @@
+package io.deeplay.qchess.client.config;
+
+import io.deeplay.qchess.client.view.gui.PlayerType;
+import io.deeplay.qchess.game.model.Color;
+import java.util.regex.Pattern;
+
+public class ConfigService {
+    /**
+     * Валидация значения полученного по ключу ip.
+     *
+     * @param property поле считанное с config файла по ключу ip.
+     * @return Результат property.
+     * @throws ConfigException выбросится если
+     *  - значение поля было пустым или ip вообще не было в config файле;
+     *  - ip состоит не из 4-х октетов;
+     *  - один из октетов не int;
+     *  - один из октетов выходит за диапазон [0..255].
+     */
+    public static String validateIp(String property) throws ConfigException {
+        if (property == null || property.isEmpty()) {
+            throw new ConfigException(
+                ConfigExceptionEnum.ABSENT_IP);
+        }
+        String[] octets = property.split("\\.");
+        if (octets.length != 4) {
+            throw new ConfigException(ConfigExceptionEnum.INCORRECT_IP_OCTETS_NUMBER);
+        }
+        for (int i = 0; i < 4; i++) {
+            try {
+                int intOctet = Integer.parseInt(octets[i]);
+                if (intOctet < 0 || intOctet > 255) {
+                    throw new ConfigException(
+                        ConfigExceptionEnum.RANGE_OUT_IP_OCTET);
+                }
+            } catch (NumberFormatException e) {
+                throw new ConfigException(
+                    ConfigExceptionEnum.INCORRECT_IP_OCTET_VALUE);
+            }
+        }
+        return property;
+    }
+
+    /**
+     * Валидация значения полученного по ключу port.
+     *
+     * @param property поле считанное с config файла по ключу port.
+     * @return Результат целочисленное представление параметра property.
+     * @throws ConfigException выбросится если
+     *  - значение поля было пустым или port вообще не было в config файле;
+     *  - port не int;
+     *  - port является неположительным числом.
+     */
+    public static int validatePort(String property) throws ConfigException {
+        if (property == null || property.isEmpty()) {
+            throw new ConfigException(
+                ConfigExceptionEnum.ABSENT_PORT);
+        }
+        try {
+            int port = Integer.parseInt(property);
+            if (port <= 0) {
+                throw new ConfigException(ConfigExceptionEnum.NON_POSITIVE_PORT_VALUE);
+            }
+            return port;
+        } catch (NumberFormatException e) {
+            throw new ConfigException(ConfigExceptionEnum.INCORRECT_PORT_VALUE);
+        }
+    }
+
+    /**
+     * Валидация boolean значения.
+     *
+     * @param property - boolean значение обёрнутое строкой.
+     * @return Результат boolean представление параметра property.
+     * @throws ConfigException выбросится если
+     *  - значение было пустым или null;
+     *  - property не явлеяется строкой "true" или "false".
+     */
+    public static boolean validateBoolean(String property) throws ConfigException {
+        if (property == null || property.isEmpty()) {
+            throw new ConfigException(
+                ConfigExceptionEnum.ABSENT_BOOLEAN);
+        }
+        if (property.equals("true")) {
+            return true;
+        } else if (property.equals("false")) {
+            return false;
+        }
+        throw new ConfigException(ConfigExceptionEnum.INCORRECT_BOOLEAN_VALUE);
+    }
+
+    /**
+     * Валидация значения полученного по ключу playerType.
+     *
+     * @param property поле считанное с config файла по ключу playerType.
+     * @return Результат одно из значений enum client.view.gui.PlayerType.
+     * @throws ConfigException выбросится если
+     *  - значение поля было пустым или playerType вообще не было в config файле;
+     *  - неудалось распарсить значение в элемент enum client.view.gui.PlayerType.
+     */
+    public static PlayerType validatePlayerType(String property) throws ConfigException {
+        if (property == null || property.isEmpty()) {
+            throw new ConfigException(
+                ConfigExceptionEnum.ABSENT_PLAYER_TYPE);
+        }
+        try {
+            return PlayerType.valueOf(property);
+        } catch (IllegalArgumentException e) {
+            throw new ConfigException(ConfigExceptionEnum.INCORRECT_PLAYER_TYPE_VALUE);
+        }
+    }
+
+    /**
+     * Валидация пути к папке
+     *
+     * @param property - строковое представление пути к папке.
+     * @return Результат property.
+     * @throws ConfigException выбросится если
+     *  - значение было пустым или null;
+     *  - путь не соответствует шаблону представления пути;
+     */
+    public static String validatePath(String property) throws ConfigException {
+        if (property == null || property.isEmpty()) {
+            throw new ConfigException(
+                ConfigExceptionEnum.ABSENT_PATH);
+        }
+        String pattern = "/([\\w]+/)*";
+        if (Pattern.matches(pattern, property)) {
+            throw new ConfigException(
+                ConfigExceptionEnum.INCORRECT_PATH);
+        }
+        return property;
+    }
+
+    /**
+     * Валидация значения полученного по ключу color.
+     *
+     * @param property поле считанное с config файла по ключу color.
+     * @return Результат одно из значений enum game.model.Color или null (нужно выбрать рандомный цвет).
+     * @throws ConfigException выбросится если
+     *  - значение поля было пустым или color вообще не было в config файле;
+     *  - property не парсится ни в один из цветов, а также не является строкой "RANDOM".
+     */
+    public static Color validateColor(String property) throws ConfigException {
+        if (property == null || property.isEmpty()) {
+            throw new ConfigException(
+                ConfigExceptionEnum.ABSENT_COLOR);
+        }
+        switch (property) {
+            case "WHITE":
+                return Color.WHITE;
+            case "BLACK":
+                return Color.BLACK;
+            case "RANDOM":
+                return null;
+        }
+        throw new ConfigException(ConfigExceptionEnum.INCORRECT_COLOR);
+    }
+}
