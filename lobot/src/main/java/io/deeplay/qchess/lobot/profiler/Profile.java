f0e27e4260ebdc48d8eac1c92fb402f70cc6fed9
Manjidomoe <x67379@gmail.com>
2021-08-20 18:32:31 +0600

    недописан профайлер

--- lobot/src/main/java/io/deeplay/qchess/lobot/profiler/Profile.java
+++ lobot/src/main/java/io/deeplay/qchess/lobot/profiler/Profile.java
@@ -1,16 +1,35 @@
 package io.deeplay.qchess.lobot.profiler;
 
 import io.deeplay.qchess.game.GameSettings;
-import io.deeplay.qchess.game.model.BoardState;
+import io.deeplay.qchess.game.exceptions.ChessError;
 import io.deeplay.qchess.game.model.Move;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Profile {
-    private final Map<BoardState, Distribution> repository;
+    private final Map<String, Distribution> repository;
+    private final String name;
 
     public Profile() {
+        this("general");
+    }
+
+    public Profile(final String name) {
         repository = new HashMap<>();
+        this.name = name;
+    }
+
+    /**
+     * Добавляет запись в профиль. Если такого ключа не было создаётся новая запись<br>
+     * Иначе обновляется значение о ключу.
+     * @param fen нотация Форсайта-Эдвардса
+     * @param move ход который добавится в значение
+     */
+    public void add(final String fen, final Move move) {
+        final Distribution distribution = repository.getOrDefault(fen, defaultBoardState());
+        repository.put(fen, distribution.setOrAddMove(move));
     }
 
     /**
@@ -19,18 +38,18 @@ public class Profile {
      * @param gs из неё мы получим состояние борды, которое используем как ключ
      * @param move ход который добавится в значение
      */
-    public void add(final GameSettings gs, final Move move) {
-        final BoardState bs = gs.history.getLastBoardState();
-        final Distribution distribution = repository.getOrDefault(bs, defaultBoardState());
-        repository.put(bs, distribution.setOrAddMove(move));
+    public void add(final GameSettings gs, final Move move) throws ChessError {
+        final String fen = gs.history.getBoardToStringForsythEdwards();
+        add(fen, move);
     }
 
     /**
      * @param gs по которому будет вернётся распределение оценок ходов
      * @return распределение оценок ходов для этого GameSetting
      */
-    public Distribution get(final GameSettings gs) {
-        return repository.getOrDefault(gs.history.getLastBoardState(), defaultBoardState());
+    public Distribution get(final GameSettings gs) throws ChessError {
+        final String fen = gs.history.getBoardToStringForsythEdwards();
+        return repository.getOrDefault(fen, defaultBoardState());
     }
 
     /**
@@ -38,5 +57,17 @@ public class Profile {
      */
     private Distribution defaultBoardState() {
         return new Distribution();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // todo посмотреть на json, tostring для distr
+    public void save(final BufferedWriter bw) throws IOException {
+        for(final String key : repository.keySet()) {
+            bw.write(key);
+            bw.write(repository.get(key).toString());
+        }
     }
 }
--- lobot/src/main/java/io/deeplay/qchess/lobot/profiler/ProfileService.java
+++ lobot/src/main/java/io/deeplay/qchess/lobot/profiler/ProfileService.java
@@ -1,7 +1,22 @@
 package io.deeplay.qchess.lobot.profiler;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ProfileService {
+    private static final File saveDirectory = new File("./lobot/src/main/resources/profiles");
+    private static final File loadDirectory = new File("./lobot/src/main/resources/profiles");
+
     public static void saveProfile(final Profile profile) {
+        final String namePath = saveDirectory + "/" + profile.getName();
+        try(final BufferedWriter bw = new BufferedWriter(new FileWriter(namePath + "/profile.csv"))) {
+            profile.save(bw);
+        } catch (final IOException e) {
+            //
+        }
     }
 
     public static Profile loadProfile() {
